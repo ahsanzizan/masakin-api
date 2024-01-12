f51dde0a127027c7b73f4c77be2254b2f59ec3c3
Ahsan <ahsanaz461@gmail.com>
2024-01-12 17:40:13 +0700

    fix(auth): Read IS_PUBLIC_KEY in the auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import SignInDto from './dto/signIn.dto';
 import SignUpDto from './dto/signUp.dto';
+import { AllowAnon } from './auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @AllowAnon()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() credentials: SignInDto) {
     return this.authService.signIn(credentials.username, credentials.password);
   }
 
+  @AllowAnon()
   @HttpCode(HttpStatus.CREATED)
   @Post('register')
   signUp(@Body() credentials: SignUpDto) {
