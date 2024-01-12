f5ca1a6d54156a3c4d6b936f871292c87821c4cd
Ahsan Awadullah Azizan <ahsanaz461@gmail.com>
2024-01-12 15:26:01 +0700

    feat: Add pagination (#15)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { paginator } from 'src/prisma/paginator';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+const paginate = paginator({ perPage: 10 });
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
+  async getUsers(
+    where?: Prisma.UserWhereInput,
+    orderBy?: Prisma.UserOrderByWithRelationInput,
+    page?: number,
+  ) {
+    return paginate(this.prisma.user, { where, orderBy }, { page });
+  }
 
   async findUserByUsername(username: string) {
     return await this.prisma.user.findUnique({ where: { username } });
