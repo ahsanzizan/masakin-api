e0e7d2543ff2e9d53b38f46fbe13f0b193f9cd24
Ahsan <ahsanaz461@gmail.com>
2024-01-12 15:38:57 +0700

    feat(auth): Apply auth guard to all routes except for the 'AllowAnon' routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
@@ -10,7 +12,13 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({ global: true, secret: process.env.JWT_SECRET }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
