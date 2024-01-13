db67d2ab8469c6a4fa939b9b3e14a5f77b8d860f
Ahsan Awadullah Azizan <ahsanaz461@gmail.com>
2024-01-13 10:14:23 +0700

    feat(auth): Add a decorator to access the payload (#21)
    * feat(auth): Add a decorator to access the payload
    
    * refactor: Mark services as readonly

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,22 +7,22 @@ const paginate = paginator({ perPage: 10 });
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   async getUsers(
     where?: Prisma.UserWhereInput,
     orderBy?: Prisma.UserOrderByWithRelationInput,
     page?: number,
   ): Promise<PaginatedResult<User>> {
-    return paginate(this.prisma.user, { page }, { where, orderBy });
+    return paginate(this.prismaService.user, { page }, { where, orderBy });
   }
 
-  async findUserByUsername(username: string) {
-    return await this.prisma.user.findUnique({ where: { username } });
+  async findByUsername(username: string) {
+    return await this.prismaService.user.findUnique({ where: { username } });
   }
 
   async createUser(data: Prisma.UserCreateInput, include?: Prisma.UserInclude) {
-    await this.prisma.user.create({
+    await this.prismaService.user.create({
       data,
       include,
     });
