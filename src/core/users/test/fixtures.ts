fb757ca0463a69b9410be4445166493d5b3f1946
Ahsan <ahsanaz461@gmail.com>
2024-01-15 05:26:01 +0700

    refactor(test): Use usersSeeder for the paginatedUsersSeeder

--- src/core/users/test/fixtures.ts
+++ src/core/users/test/fixtures.ts
@@ -30,31 +30,9 @@ export const usersSeeder: User[] = [
 ];
 
 export const paginatedUsersSeeder: PaginatedResult<UserWithoutPasswordType> = {
-  data: [
-    {
-      id: '70d1c5a7-febf-4600-a64f-3dfa68041cc5',
-      username: 'Test User 1',
-      email: 'testing1@user.com',
-      createdAt: new Date(),
-      bio: null,
-    },
-    {
-      id: '20d1c5a7-febf-4600-a64f-35fx68041cc5',
-      username: 'Test User 2',
-      email: 'testing2@user.com',
-      createdAt: new Date(),
-      bio: null,
-    },
-    {
-      id: '70d1d5n7-feby-46a0-b6nf-3dfa68041cc5',
-      username: 'Test User 3',
-      email: 'testing3@user.com',
-      createdAt: new Date(),
-      bio: null,
-    },
-  ],
+  data: usersSeeder.map((user) => ({ ...user, password: undefined })),
   meta: {
-    total: 3,
+    total: usersSeeder.length,
     lastPage: 1,
     currentPage: 1,
     perPage: 10,
